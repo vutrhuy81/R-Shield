8d5b5dbafc08dfd427b1fd8f4161803a73cc2884
Tran Huy Vu <vutrhuy81@gmail.com>
2025-12-31 10:41:30 +0700

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -13,3 +13,16 @@ export const MOCK_INITIAL_TERMS = [
   "ăn thịt người",
   "Lạng Sơn"
 ];
+
+export const K_FACTOR = 6000;
+
+export const DEFAULT_REAL_DATA = [
+  { day: 0, real_I: 5000 },
+  { day: 1, real_I: 30000 },
+  { day: 2, real_I: 150000 },
+  { day: 3, real_I: 450000 },
+  { day: 4, real_I: 600000 },
+  { day: 5, real_I: 350000 },
+  { day: 6, real_I: 120000 },
+  { day: 7, real_I: 50000 },
+];
