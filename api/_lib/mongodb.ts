0b9447f78389e08ed3f74ec74bdd53f3c2833ac5
Tran Huy Vu <vutrhuy81@gmail.com>
2026-04-05 13:36:17 +0700

    Update mongodb.ts

--- api/_lib/mongodb.ts
+++ api/_lib/mongodb.ts
@@ -1,1 +1,26 @@
 
+// api/_lib/mongodb.ts
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error('Vui lòng định nghĩa MONGODB_URI trong file .env');
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
